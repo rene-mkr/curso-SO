4731758bd332f6110385883532a4119510e94770
rene-mkr <rene.mkr@gmail.com>
2020-09-27 20:57:05 -0500

    Update hexEditor.c
    Se agrega leeChar()

--- hexEditor.c
+++ hexEditor.c
@@ -57,6 +57,25 @@ int mapFile(char *filePath) {
   return 0;
 }
 
+int leeChar() {
+  int chars[5];
+  int ch,i=0;
+  nodelay(stdscr, TRUE);
+  while((ch = getch()) == ERR); /* Espera activa */
+  ungetch(ch);
+  while((ch = getch()) != ERR) {
+    chars[i++]=ch;
+  }
+  /* convierte a numero con todo lo leido */
+  int res=0;
+  for(int j=0;j<i;j++) {
+    res <<=8;
+    res |= chars[j];
+  }
+  return res;
+}
+
+
 int main() {
     initscr();
     cbreak();
