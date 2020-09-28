95bf156237ff5cdf6db59df74713c4260ac5200e
rene-mkr <rene.mkr@gmail.com>
2020-09-27 20:47:50 -0500

    Create hexEditor.c
    Editor hexadecimal, versión inicial

---
+++ hexEditor.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
+#include <curses.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+char *hazLinea(char *base, int dir) {
+	char linea[100]; // La linea es mas pequeña
+	int o=0;
+	// Muestra 16 caracteres por cada linea
+	o += sprintf(linea,"%08x ",dir); // offset en hexadecimal
+	for(int i=0; i < 4; i++) {
+		unsigned char a,b,c,d;
+		a = base[dir+4*i+0];
+		b = base[dir+4*i+1];
+		c = base[dir+4*i+2];
+		d = base[dir+4*i+3];
+		o += sprintf(&linea[o],"%02x %02x %02x %02x ", a, b, c, d);
+	}
+	for(int i=0; i < 16; i++) {
+		if (isprint(base[dir+i])) {
+			o += sprintf(&linea[o],"%c",base[dir+i]);
+		}
+		else {
+			o += sprintf(&linea[o],".");
+		}
+	}
+	sprintf(&linea[o],"\n");
+
+	return(strdup(linea));
+}
+
+int mapFile(char *filePath) {
+    /* Abre archivo */
+    int fd = open(filePath, O_RDONLY);
+    if (fd == -1) {
+    	perror("Error abriendo el archivo");
+	    return(EXIT_FAILURE);
+    }
+
+    /* Mapea archivo */
+    struct stat st;
+    fstat(fd,&st);
+    int fs = st.st_size;
+
+    char *map = mmap(0, fs, PROT_READ, MAP_SHARED, fd, 0);
+    if (map == MAP_FAILED) {
+    	close(fd);
+	    perror("Error mapeando el archivo");
+	    return(EXIT_FAILURE);
+    }
+
+  return 0;
+}
+
+int main() {
+    initscr();
+    cbreak();
+    noecho();
+    
+    /* Lee archivo */
+    if (mapFile( "DISK1.DSK") != 0) {
+      exit(EXIT_FAILURE);
+      }
+    
+    for(int i= 0; i<25; i++) {
+    	// Haz linea, base y offset
+    	char *l = hazLinea(map,i*16);
+	    mvprintw(i,0,l);
+    }
+    refresh();
+
+    leeChar();
+
+    return(0);
+}
+    
+
+
