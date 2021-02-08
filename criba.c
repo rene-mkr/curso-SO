011ce69a5a89ae667f2e8c21d6fca2db7878ea78
rene-mkr <rene.mkr@gmail.com>
2021-02-08 09:19:28 -0600

    Create criba.c

---
+++ criba.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  
+int main() {
+    int i,n,prims[n],x=0,p,nums[n];
+    n = 19000; // número hasta el que se quiere buscar
+    for(i=2;i<=n;i++) {
+        if(nums[i] != 1 || i == 2) {
+            prims[x] = i;
+            printf("El número %d es prim",i);
+            for(p=2;(p*i)<=n;p++) {
+                nums[(p*i)] = 1;
+            }
+            x++;
+        }
+    }
+    printf("Hay %d números primos",x);
